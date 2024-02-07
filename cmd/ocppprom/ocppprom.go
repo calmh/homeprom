b30fd91a593ccdd14a43db34ce7c3b97a7e2f4ef
Jakob Borg <jakob@kastelo.net>
2024-02-07 13:19:04 +0100

    wip

---
+++ cmd/ocppprom/ocppprom.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/aliml92/ocpp"
+	v16 "github.com/aliml92/ocpp/v16"
+)
+
+var csms *ocpp.Server
+
+// log replaces standard log
+var log *zap.SugaredLogger
+
+func main() {
+	// start csms server with default configurations
+	csms = ocpp.NewServer()
+
+	csms.AddSubProtocol("ocpp1.6")
+	csms.SetCheckOriginHandler(func(r *http.Request) bool { return true })
+	csms.SetPreUpgradeHandler(customPreUpgradeHandler)
+	csms.SetCallQueueSize(32)
+
+	// register charge-point-initiated action handlers
+	csms.On("BootNotification", BootNotificationHandler)
+	// csms.After("BootNotification", SendChangeConfigration)
+	csms.On("Authorize", AuthorizationHandler)
+	csms.Start("0.0.0.0:8999", "/ws/", nil)
+}
+
+// func SendChangeConfigration(cp *ocpp.ChargePoint, payload ocpp.Payload) {
+// 	var req ocpp.Payload = v16.ChangeConfigurationReq{
+// 		Key:   "WebSocketPingInterval",
+// 		Value: "30",
+// 	}
+// 	res, err := cp.Call("ChangeConfiguration", req)
+// 	if err != nil {
+// 		log.Debug(err)
+// 	}
+// 	log.Debug(res)
+// }
+
+func customPreUpgradeHandler(w http.ResponseWriter, r *http.Request) bool {
+	u, p, ok := r.BasicAuth()
+	if !ok {
+		log.Debug("error parsing basic auth")
+		w.WriteHeader(401)
+		return false
+	}
+	path := strings.Split(r.URL.Path, "/")
+	id := path[len(path)-1]
+	log.Debugf("%s is trying to connect with %s:%s", id, u, p)
+	if u != id {
+		log.Debug("username provided is correct: %s", u)
+		w.WriteHeader(401)
+		return false
+	}
+	return true
+}
+
+func BootNotificationHandler(cp *ocpp.ChargePoint, p ocpp.Payload) ocpp.Payload {
+	req := p.(*v16.BootNotificationReq)
+	log.Debugf("\nid: %s\nBootNotification: %v", cp.Id, req)
+
+	var res ocpp.Payload = &v16.BootNotificationConf{
+		CurrentTime: time.Now().Format("2006-01-02T15:04:05.000Z"),
+		Interval:    60,
+		Status:      "Accepted",
+	}
+	return res
+}
+
+func AuthorizationHandler(cp *ocpp.ChargePoint, p ocpp.Payload) ocpp.Payload {
+	req := p.(*v16.AuthorizeReq)
+	log.Debugf("\nid: %s\nAuthorizeReq: %v", cp.Id, req)
+
+	var res ocpp.Payload = &v16.AuthorizeConf{
+		IdTagInfo: v16.IdTagInfo{
+			Status: "Accepted",
+		},
+	}
+	return res
+}
